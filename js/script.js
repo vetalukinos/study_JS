1233e3e050da658f9b01ad242a3367bc450c946f
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-03-29 15:30:06 +0300

    first lesson

---
+++ js/script.js
@@ -0,0 +1,18 @@
+
+const money = 10;
+
+let income = 10;
+
+const addExpenses = 5;
+
+let deposit = 10;
+
+const mission = 'good';
+
+let period = 5;
+
+//alert
+alert('Show must go on!');
+
+/*console.log*/
+console.log("It's raining men! Hallelujah! It's raining men! amen!");
