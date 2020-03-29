2f11323ccfc7deb2b7f7495ee4a86a6601c6f806
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-03-29 17:32:25 +0300

    lesson 2

--- js/script.js
+++ js/script.js
@@ -1,19 +1,35 @@
 
-let money;
+let money = 60;
+console.log(typeof money);
 
-let income;
+let income = 'freelance';
+console.log(typeof income);
 
-let addExpenses;
+let addExpenses = 'Internet, Taxi, Communal';
+console.log(addExpenses.length);
+console.log(addExpenses.toLowerCase().split(', '));
 
-let deposit;
+let deposit = true;
+console.log(typeof deposit);
 
-let mission;
+let mission = 10000;
 
-let period;
+let period = 12;
+
+console.log('Период равен ' + period + ' месяцев');
+
+console.log('Цель заработать ' + mission + ' долларов');
+
+let budgetDay = money/30;
+console.log(budgetDay);
 
 
-//alert
-alert('Show must go on!');
 
-/*console.log*/
-console.log("It's raining men! Hallelujah! It's raining men! amen!");
+
+
+
+
+
+
+
+
