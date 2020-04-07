56b0c0be819144e9a424e3fa4449c0c4d2699b22
@vitaliylukin91 <vitaliylukin91@gmail.com>
2020-04-07 22:29:57 +0300

    fix : bug

--- js/script.js
+++ js/script.js
@@ -15,25 +15,25 @@ console.log(depositCheck);
 let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
 console.log(additionalIncomeItem);
 
-let budgetMonthValue = document.getElementsByClassName('budget_month-value');
+let budgetMonthValue = document.getElementsByClassName('budget_month-value')[0];
 console.log(budgetMonthValue);
 
-let budgetDayValue = document.getElementsByClassName('budget_day-value');
+let budgetDayValue = document.getElementsByClassName('budget_day-value')[0];
 console.log(budgetDayValue);
 
-let expensesMonthValue = document.getElementsByClassName('expenses_month-value');
+let expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0];
 console.log(expensesMonthValue);
 
-let additionalIncomeValue = document.getElementsByClassName('additional_income-value');
+let additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0];
 console.log(additionalIncomeValue);
 
-let additionalExpensesValue = document.getElementsByClassName('additional_expenses-value');
+let additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0];
 console.log(additionalExpensesValue);
 
-let incomePeriodValue = document.getElementsByClassName('income_period-value');
+let incomePeriodValue = document.getElementsByClassName('income_period-value')[0];
 console.log(incomePeriodValue);
 
-let targetMonthValue = document.getElementsByClassName('target_month-value');
+let targetMonthValue = document.getElementsByClassName('target_month-value')[0];
 console.log(targetMonthValue);
 
 let salaryAmount = document.querySelector('.salary-amount');
